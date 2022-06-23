f4ab9fdeaeb2461edb1214ca7a6a2bd2ba7ed70e
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2022-06-23 23:37:24 +0900

    Update cal.js

--- js/cal.js
+++ js/cal.js
@@ -1,5 +1,5 @@
 function cal() {
   var x = Number(document.getElementById('inputValue').value);
   var a = Math.ceil(x * 500 / 467);
-  document.getElementById("outputCal").value = a;
+  document.getElementById("outputCal").value = Math.ceil(a);
 }
